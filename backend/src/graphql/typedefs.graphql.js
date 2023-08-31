f9c28d6acccc7df3309d19017ccc6c241d99a7ee
cxnturi0n <f.cinicolo@studenti.unina.it>
2023-08-31 23:15:42 +0200

    Formatted typedefs

--- backend/src/graphql/typedefs.graphql.js
+++ backend/src/graphql/typedefs.graphql.js
@@ -1,33 +1,33 @@
-const observationTypedefs = `#graphql
-
+const observationTypedefs = `
+  #graphql
   enum Order {
     ASC
     DESC
   }
 
-  enum Sort{
+  enum Sort {
     road_id
     timestamp
     avg_speed
     num_vehicles
   }
-  
-  enum Region{
+
+  enum Region {
     Anderlecht
     Belgium
     Bruxelles
   }
-  
+
   type RoadSumVehiclesAggregation {
     road_id: Int!
     sum_vehicles: Int!
   }
-  
+
   type RoadAvgSpeedAggregation {
     road_id: Int!
     avg_speed: Float!
   }
-  
+
   input ObservationsWhere {
     region: Region!
     granularity: Int!
@@ -36,73 +36,48 @@ const observationTypedefs = `#graphql
     roadIds: [Int]
     days: [Int]
   }
-  
-  input RoadGeometriesWhere{
+
+  input RoadGeometriesWhere {
     region: String!
     roadIds: [Int]
   }
-  
-  input RoadsCountWhere{
+
+  input RoadsCountWhere {
     region: Region!
   }
-  
-  input AggregationObservationsOptions{
+
+  input AggregationObservationsOptions {
     limit: Int
     order: Order
   }
 
-  input ObservationsOptions{
+  input ObservationsOptions {
     limit: Int
     sort: Sort
     order: Order
   }
 
-  type Coordinate {
-    latitude: Float!
-    longitude: Float!
-  }
-  
-  type LinearRing {
-    coordinates: [Coordinate!]!
-  }
-  
-  type Polygon {
-    rings: [LinearRing!]!
-  }
-  
-  type Road {
+  type Observation {
     road_id: Int!
-    polygons: [Polygon]!
+    timestamp: Int!
+    avg_speed: Float!
+    num_vehicles: Int!
   }
-  
-    type Observation { 
-      road_id: Int!
-      timestamp: Int!
-      avg_speed: Float!
-      num_vehicles: Int!
-    }
 
-  
   type Query {
-
     observations(where: ObservationsWhere, options: ObservationsOptions): [Observation!]
-
+    
     vehicleCountForEachRoad(
-      where: ObservationsWhere,
+      where: ObservationsWhere
       options: AggregationObservationsOptions
     ): [RoadSumVehiclesAggregation]!
-  
+
     averageSpeedForEachRoad(
-      where: ObservationsWhere,
+      where: ObservationsWhere
       options: AggregationObservationsOptions
     ): [RoadAvgSpeedAggregation]!
-  
-    roads(where: RoadGeometriesWhere): [Road!]!
-    roadsCount(where: RoadsCountWhere): Int!
+
   }
-  
-
-
 `;
 
 export { observationTypedefs };
